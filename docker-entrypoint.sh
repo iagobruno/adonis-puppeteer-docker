762a93e1002227ae8543f0cb161332fbf786f71a
Iago Bruno <iagobruno.dev@gmail.com>
2021-07-20 01:46:28 -0300

    fix: Allow set which app version will run inside the container

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
-echo "Starting Adonis server..."
+
 # node ace migration:rollback --batch 0
 node ace migration:run
-# node ace db:seed
-node ace serve --watch
+
+echo "Starting Adonis server..."
+
+if [ ${NODE_ENV} = "production" ]; then
+  yarn run build
+  yarn run start
+else
+  yarn run dev
+fi
